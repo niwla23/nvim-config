c95e1d38503f3eba75b45e201840d108f4564803
niwla23 <46248939+niwla23@users.noreply.github.com>
2024-01-25 22:05:00 +0100

    ignore node_modules

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,7 +2,7 @@ require("telescope").setup({
   pickers = {
     find_files = {
       hidden = true,
-      file_ignore_patterns = { ".git/", ".cache", "%.o", "%.a", "%.out", "%.class",
+      file_ignore_patterns = { ".git/", "node_modules", ".cache", "%.o", "%.a", "%.out", "%.class",
         "%.pdf", "%.mkv", "%.mp4", "%.zip" },
     }
   }
